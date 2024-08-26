6a5722db8034e8dbf44454898cc95efeb6847cf3
luislopez-developer <luislopezhuari27@gmail.com>
2024-08-26 16:55:03 -0500

    add new activity_register.xml

--- app/src/main/java/com/example/semana2_senati/data/User.java
+++ app/src/main/java/com/example/semana2_senati/data/User.java
@@ -1,6 +1,7 @@
 package com.example.semana2_senati.data;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
