c6e46199cc2db998e78990bfc35a087ce5166e08
luislopez-developer <luislopezhuari27@gmail.com>
2024-08-26 15:03:21 -0500

    add product register

--- app/src/main/java/com/example/semana2_senati/DataBaseActivity.java
+++ app/src/main/java/com/example/semana2_senati/DataBaseActivity.java
@@ -2,10 +2,14 @@ package com.example.semana2_senati;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.semana2_senati.data.Administrador;
 import com.example.semana2_senati.databinding.ActivityDataBaseBinding;
-import com.example.semana2_senati.databinding.ActivityMainBinding;
 
 public class DataBaseActivity extends AppCompatActivity {
 
@@ -17,5 +21,36 @@ public class DataBaseActivity extends AppCompatActivity {
 
         binding = ActivityDataBaseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.btnProductRegister.setOnClickListener(v -> registerPorduct(v));
     }
+
+    private void registerPorduct(View v) {
+        Administrador admin = new Administrador(this, "administracion", null, 1);
+
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String code = binding.txtCode.getText().toString();
+        String description = binding.txtDescription.getText().toString();
+        String price = binding.txtPrice.getText().toString();
+
+        if(!code.isEmpty() && !description.isEmpty() && !price.isEmpty()){
+            ContentValues register = new ContentValues();
+            register.put("codido", code);
+            register.put("description", code);
+            register.put("precio", code);
+
+            db.insert("articulos", null, register);
+            db.close();
+
+            binding.txtCode.setText("");
+            binding.txtDescription.setText("");
+            binding.txtPrice.setText("");
+
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Debes llenar tpdos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
